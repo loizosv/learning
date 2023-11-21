92dbe2cd6987d6f31b4e9b1139a59a9dd8a691e5
Loizos Vasileiou <loizos.vas@hotmail.com>
2023-11-21 13:51:46 +0400

    project6
    adding files to Learning React Project 6

---
+++ react/coursera_learn_react/project6/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <nav>
+          <h1>Bob's Bistro</h1>
+          <ul>
+              <li>Menu</li>
+              <li>About</li>
+              <li>Contact</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
